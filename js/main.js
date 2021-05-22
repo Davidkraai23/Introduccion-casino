4cf1acef8f47a8fc5813c44b8f809ae98cdf6868
Davidkraai23 <davidbohorquezleal@gmail.com>
2021-05-21 21:50:06 -0500

    java 2

--- js/main.js
+++ js/main.js
@@ -11,3 +11,34 @@ window.onload = init;
 function init(){
 boton_tirar = document.getElementById("boton_tirar");
 boton_tirar.addEventListener("click", jugar);
+dado1 = document.getElementById("dado1");
+dado2 = document.getElementById("dado2");
+}
+
+function tirardado(){
+    return Math.floor(Math.random() *6) +1 ;
+}
+
+function actualizarDado(ref,cara){
+    ref.style.backgroundPosition = posiciones[cara-1]+"px";
+}
+
+function jugar(){
+    tiro_1 = tirardado();//Retorna un numero entre 1 y 6
+    tiro_2 = tirardado();
+    actualizarDado(dado1, tiro_1);
+    actualizarDado(dado2, tiro_2);
+
+    console.log("turno actual",turno);
+    total_t = tiro_1 + tiro_2;
+    console.log(" Tiro= "+total_t)
+
+    if(turno==1 && (total_t==7 || total_t==11))
+    {
+        turno = 1;
+
+        console.log("Reinicia")
+
+        alert("Ganaste con:  " + total_t + " Suerte c:");
+        window.location.reload();
+    }
